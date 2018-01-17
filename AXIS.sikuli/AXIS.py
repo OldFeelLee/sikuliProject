0a06ac8b8a53ea92d3328450bc00b13695538125
leehunpill <leehunpill@idis.co.kr>
2018-01-17 15:48:57 +0900

    axis newline 제거

--- AXIS.sikuli/AXIS.py
+++ AXIS.sikuli/AXIS.py
@@ -3,9 +3,6 @@ def doTest() :
     global errorReason
     try:
         click("1437728549609.png")
-        
- 
-
         return True
     except FindFailed :
         errorReason = "no target resolution or framerate"

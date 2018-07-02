4811d9424b01eb29766adad691d3e4ec34bd46d0
leehunpill <leehunpill@idis.co.kr>
2018-07-02 13:58:20 +0900

    .

--- clickFunc.sikuli/clickFunc.py
+++ clickFunc.sikuli/clickFunc.py
@@ -1,8 +1,12 @@
 from sikuli import *
+import fileLog
+
 
 def click_click(img1,img2):
-    click(img1) 
+    fileLog.status = "click1"    
+    click(img1)
     wait(2)
+    fileLog.status = "click2"
     click(img2)
     wait(2)
 
@@ -12,18 +16,34 @@ def click_wait_click(img1,img2,waitTime):
     click(img2)
     wait(waitTime)
 
+def click_wait_doubleClick(img1,img2,waitTime):
+    click(img1)
+    wait(waitTime)
+    doubleClick(img2)
+    wait(waitTime)
+
 def click_type(img1,str):
     click(img1)
     wait(3)
     type(str)
     wait(3)
 
-def click_type_type(img1,str1,str2):
+
+def click_type_hover_(img1,str,hoverImg):
     click(img1)
     wait(3)
-    type(str1)
+    type(str)
     wait(3)
-    type(str2)
+    hover(hoverImg)
+    wait(3)
+
+
+def click_type_hover(img1,str,hoverImg):
+    click(img1)
+    wait(3)
+    type(str)
+    wait(3)
+    hover(hoverImg)
     wait(3)
 
 
@@ -33,4 +53,12 @@ def click_click_click(img1,img2,img3):
     click(img2)
     wait(2)
     click(img3)
+    wait(2)
+
+def click_click_type(img1, img2, str1):
+    click(img1)
+    wait(2)
+    click(img2)
+    wait(2)
+    type(str1)
     wait(2)

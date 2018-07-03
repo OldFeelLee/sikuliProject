ca06ee4863c8ef108a054a7d5ada42081cf588f7
leehunpill <leehunpill@idis.co.kr>
2018-07-03 10:12:37 +0900

    .

--- existsFunc.sikuli/existsFunc.py
+++ existsFunc.sikuli/existsFunc.py
@@ -18,10 +18,27 @@ def whileNotExists_wait(showImage):
     while not exists(showImage):
         wait(2)
 
+def exists_click(showimg):
+    if exists(showimg,30):
+        click(showimg)
+        wait(2)
+
+def exists_type(showimg,str1):
+    if exists(showimg,300):
+        type(str1)
+
+
 def exists_click_click(showimg,clickimg1,clickimg2):
     if exists(showimg):
         click(clickimg1)
         wait(2)
         click(clickimg2)
         wait(2)
-        
+
+def exists_click_type(showimg,clickimg1,str1):
+    if exists(showimg,60):
+        click(clickimg1)
+        wait(2)
+        type(str1)
+        wait(2)
+        return True        
